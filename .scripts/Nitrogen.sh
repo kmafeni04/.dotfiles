cd37db66ab066fdd24071492d85c5eb409a150ca
kmafeni04 <komemafeni944@gmail.com>
2025-04-18 23:55:41 +0100

    update

--- .scripts/Nitrogen.sh
+++ .scripts/Nitrogen.sh
@@ -5,7 +5,8 @@ wallpaper_dir="/home/kome/Pictures/Wallpapers"
 while true; do
     wallpaper=$(find "$wallpaper_dir" -type f | shuf -n 1)
     cp $wallpaper ~/.scripts/current_wallpaper.png
-    nitrogen --set-scaled "$wallpaper"
+    nitrogen --set-scaled "$wallpaper" --head=0
+    nitrogen --set-scaled "$wallpaper" --head=1
     sleep 60
 done
 
--- .scripts/player_song.sh
+++ .scripts/player_song.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-last_player="$(tail -1 /tmp/player-last)"
-
-if	[ "$(playerctl -p $last_player status)" = "Playing" ]; then
-	title=`exec playerctl -p $last_player metadata xesam:title`
-	artist=`exec playerctl -p $last_player metadata xesam:artist`
+if	[ "$(playerctl -p playerctld status -s)" = "Playing" ]; then
+	title=`exec playerctl -p playerctld metadata xesam:title`
+	artist=`exec playerctl -p playerctld metadata xesam:artist`
 	( echo "$title - $artist" ) &
-
-elif	[ "$(playerctl -p $last_player status)" = "Paused" ]; then
-	title=`exec playerctl -p $last_player metadata xesam:title`
-	artist=`exec playerctl -p $last_player metadata xesam:artist`
+elif	[ "$(playerctl -p playerctld status -s)" = "Paused" ]; then
+	title=`exec playerctl -p playerctld metadata xesam:title`
+	artist=`exec playerctl -p playerctld metadata xesam:artist`
 	( echo "(PAUSED) $title - $artist" ) &
 else
 	echo ""
