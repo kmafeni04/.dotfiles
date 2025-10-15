94364fb63fbcbd2423010aab25ef48afb55e397d
kmafeni04 <komemafeni944@gmail.com>
2025-10-15 22:36:00 +0100

    Reorder commands in swaybg.sh

--- .scripts/swaybg.sh
+++ .scripts/swaybg.sh
@@ -3,9 +3,9 @@
 wallpaper_dir="/home/kome/Pictures/Wallpapers"
 
 while true; do
-    killall swaybg
     wallpaper=$(find "$wallpaper_dir" -type f | shuf -n 1)
     cp $wallpaper ~/.scripts/current_wallpaper.png
+    killall swaybg
     swaybg -m fill -i "$wallpaper" -o 0 &
     swaybg -m fill -i "$wallpaper" -o 1 &
     sleep 60
