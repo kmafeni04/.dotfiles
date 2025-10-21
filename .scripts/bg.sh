5246b53b8c28a41125749c2db5f4170a54345dd0
kmafeni04 <komemafeni944@gmail.com>
2025-10-21 12:32:14 +0100

    Fix spacing in bg.sh

--- .scripts/bg.sh
+++ .scripts/bg.sh
@@ -3,18 +3,18 @@
 wallpaper_dir="/home/kome/Pictures/wallpapers"
 
 while true; do
-    wallpaper=$(find "$wallpaper_dir" -type f | shuf -n 1)
-    cp $wallpaper ~/.scripts/current_wallpaper.png
+  wallpaper=$(find "$wallpaper_dir" -type f | shuf -n 1)
+  cp $wallpaper ~/.scripts/current_wallpaper.png
 
-    if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-        killall swaybg
-        swaybg -m fill -i "$wallpaper" -o 0 &
-        swaybg -m fill -i "$wallpaper" -o 1 &
-    else
-        nitrogen --set-scaled "$wallpaper" --head=0
-        nitrogen --set-scaled "$wallpaper" --head=1
-    fi
-    sleep 60
+  if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+    killall swaybg
+    swaybg -m fill -i "$wallpaper" -o 0 &
+    swaybg -m fill -i "$wallpaper" -o 1 &
+  else
+    nitrogen --set-scaled "$wallpaper" --head=0
+    nitrogen --set-scaled "$wallpaper" --head=1
+  fi
+  sleep 60
 done
 
 
