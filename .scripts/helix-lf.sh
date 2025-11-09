04763fe2cbc482c064d9400e37b40586731f2f82
kmafeni04 <komemafeni944@gmail.com>
2025-11-09 23:42:39 +0100

    Open lf at file location in helix-lf script

--- .scripts/helix-lf.sh
+++ .scripts/helix-lf.sh
@@ -10,5 +10,5 @@ wezterm cli activate-pane --pane-id $pane_id
 program=$(wezterm cli list | awk -v pane_id="$pane_id" '$3==pane_id { print $6 }')
 
 if [ "$program" != "lf" ]; then
-  echo "lf" | wezterm cli send-text --pane-id $pane_id --no-paste
+  echo "lf $1" | wezterm cli send-text --pane-id $pane_id --no-paste
 fi
