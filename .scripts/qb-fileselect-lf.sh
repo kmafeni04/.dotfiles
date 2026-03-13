bcc54e8ba9ac9b50c8ad1187e019e68a984742c2
kmafeni04 <komemafeni944@gmail.com>
2026-03-13 13:58:05 +0100

    Fix qb-fileselect script

--- .scripts/qb-fileselect-lf.sh
+++ .scripts/qb-fileselect-lf.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/env bash
-# qb-fileselect-lf.sh
-# set euo pipefail
 
-case "$1" in
-  file)
-    thetype="file"
-  ;;
-  files)
-    thetype="file"
-  ;;
-esac
-
+set -ex
 
 tmpfile=$(mktemp)
-CMD="lf -selection-path $tmpfile"
-wezterm -e lf -selection-path $tmpfile &
-
-line_count=$(cat $tmpfile | wc -l)
-if [ $line_count -gt 1 ] && [ thetype == 'file' ]; then
-  notify-send -u critical -i "you choose > 1 file and you should only choose one"
-  exit 0
-fi
-
+terminator -x lf -selection-path $tmpfile
 cat $tmpfile
+rm -rf $tmpfile
