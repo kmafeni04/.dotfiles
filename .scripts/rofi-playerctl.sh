ade94934e6937926f721caa4ee4edcab226757ce
kmafeni04 <komemafeni944@gmail.com>
2026-03-13 13:58:11 +0100

    Format script

--- .scripts/rofi-playerctl.sh
+++ .scripts/rofi-playerctl.sh
@@ -10,13 +10,13 @@ options="$toggle\n$next\n$prev"
 
 chosen="$(echo -e "$options" | rofi -show -p "Player control" -dmenu -selected-row 0)"
 case $chosen in
-    $toggle)
-    playerctl play-pause
-        ;;
-    $next)
-		playerctl next
-        ;;
-    $prev)
-    playerctl previous
-        ;;
-    esac
+$toggle)
+  playerctl play-pause
+  ;;
+$next)
+  playerctl next
+  ;;
+$prev)
+  playerctl previous
+  ;;
+esac
