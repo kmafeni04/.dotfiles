91fdda616c4d9f018349c83dd9436ef223d0bffb
kmafeni04 <komemafeni944@gmail.com>
2026-03-13 13:57:42 +0100

    Format script

--- .scripts/player_song.sh
+++ .scripts/player_song.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if	[ "$(playerctl -p playerctld status -s)" = "Playing" ]; then
-	title=`exec playerctl -p playerctld metadata xesam:title`
-	artist=`exec playerctl -p playerctld metadata xesam:artist`
-	( echo "$title - $artist" ) &
-elif	[ "$(playerctl -p playerctld status -s)" = "Paused" ]; then
-	title=`exec playerctl -p playerctld metadata xesam:title`
-	artist=`exec playerctl -p playerctld metadata xesam:artist`
-	( echo "(PAUSED) $title - $artist" ) &
+if [ "$(playerctl -p playerctld status -s)" = "Playing" ]; then
+  title=$(exec playerctl -p playerctld metadata xesam:title)
+  artist=$(exec playerctl -p playerctld metadata xesam:artist)
+  (echo "$title - $artist") &
+elif [ "$(playerctl -p playerctld status -s)" = "Paused" ]; then
+  title=$(exec playerctl -p playerctld metadata xesam:title)
+  artist=$(exec playerctl -p playerctld metadata xesam:artist)
+  (echo "(PAUSED) $title - $artist") &
 else
-	echo ""
+  echo ""
 fi
