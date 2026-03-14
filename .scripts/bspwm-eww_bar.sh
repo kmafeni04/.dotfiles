533a826ecb0d686a1ffd6c215521f181cfb86a6f
kmafeni04 <komemafeni944@gmail.com>
2026-03-14 23:23:35 +0100

    Improve bspwm-eww_bar script

--- .scripts/bspwm-eww_bar.sh
+++ .scripts/bspwm-eww_bar.sh
@@ -2,10 +2,42 @@
 
 set -x
 
-bspc subscribe node_state | while read -r _ _ _ _ state flag; do
-    if [ "$state" != "fullscreen" ]; then
-        continue
-    fi
-    [ "$flag" = "on" ] && eww close-all || eww open bar
-done
+bspc subscribe node_{state,focus} desktop_focus | while read -a line; do
+  subscribed="${line[0]}"
 
+  case "$subscribed" in
+  "node_state")
+    state="${line[4]}"
+    flag="${line[5]}"
+    [ "$state" != "fullscreen" ] && continue
+
+    if [ "$flag" = "on" ]; then
+      eww close-all
+      eww_open="false"
+    else
+      eww open bar
+      eww_open="true"
+    fi
+    ;;
+  "node_focus")
+    node="${line[3]}"
+    state=$(bspc query -T -n "$node" | jq .client.state -r)
+
+    if [ "$state" = "fullscreen" ]; then
+      eww close-all
+      eww_open="false"
+    elif [ "$eww_open" = "false" ]; then
+      eww open bar
+      eww_open="true"
+    fi
+    ;;
+  "desktop_focus")
+    desktop="${line[2]}"
+    root=$(bspc query -T -d "$desktop" | jq .root -r)
+    if [ "$root" = "null" ] && [ "$eww_open" = "false" ]; then
+      eww open bar
+      eww_open="true"
+    fi
+    ;;
+  esac
+done
