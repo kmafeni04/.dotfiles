a8a8caad89fa035cbeb3a43f1ad5fda5ebc95fbb
kmafeni04 <komemafeni944@gmail.com>
2026-03-17 22:51:26 +0100

    Majorly improve bspwm-master-stack script

--- .scripts/bspwm-master-stack.sh
+++ .scripts/bspwm-master-stack.sh
@@ -8,7 +8,7 @@ bspc subscribe node_{remove,add,state,flag} desktop_focus | while read -a line; 
 
   event="${line[0]}"
 
-  desktop=$(bspc query -D -d focused)
+  desktop="$(bspc query -D -d focused)"
   [ -z "$desktop" ] && continue
 
   # Get tiled windows
@@ -21,37 +21,39 @@ bspc subscribe node_{remove,add,state,flag} desktop_focus | while read -a line; 
   master="${windows[0]}"
   bspc node "$master" -n "@/1"
 
-  for ((i = 1; i < count; i++)); do
-    slave="${windows[$i]}"
-    bspc node "$slave" -n "@/2"
-  done
+  # This makes sure all new windows are added at the end of the stack
+  if [ "$event" = "node_add" ]; then
+    new_window="${line[4]}"
+    new_window_found=false
+    for ((i = 1; i < count; i++)); do
+      if [ "${windows[$i]}" = "$new_window" ]; then
+        new_window_found=true
+        unset 'windows[i]'
+        break
+      fi
+    done
 
-  if [ "$event" = "node_add" ] && ([ "${line[3]}" = "$master" ] || [ "$(bspc query -T -n ${line[3]} | jq .client.state -r)" != "tiled" ]); then
-    win="${line[4]}"
-
-    last=$(bspc query -N -d "$desktop" -n '.window.tiled' | tail -1)
-
-    # Preselect south
-    bspc node "$last" -p south
-
-    # Move window there
-    bspc node "$win" -n "${last}"
-
-    # Cancel preselection
-    bspc node "$last" -p cancel
+    if [ "$new_window_found" = true ]; then
+      windows=("${windows[@]}") # Shift the array
+      windows[$((count - 1))]="$new_window"
+    fi
   fi
 
-  # Balance to equal widths in stack
+  for ((i = 1; i < count; i++)); do
+    bspc node "${windows[$i]}" -n "@/2"
+  done
+
+  # Balance the slave window stack
   bspc node "@/2" -B
 
   # Get monitor width and calculate
-  mon_width=$(bspc query -T -m | jq .rectangle.width -r)
+  mon_width="$(bspc query -T -m | jq .rectangle.width -r)"
   master_width=$((mon_width * MASTER_SIZE / 100))
 
   # Get current master window width
-  current_master_width=$(bspc query -T -n "$master" | jq .rectangle.width -r)
+  current_master_width="$(bspc query -T -n "$master" | jq .rectangle.width -r)"
 
-  # Resize if needed
+  # Resize master if needed
   if [ -n "$current_master_width" ] && [ -n "$master_width" ]; then
     diff=$((master_width - current_master_width))
     if [ "$diff" -gt 10 ] || [ "$diff" -lt -10 ]; then
