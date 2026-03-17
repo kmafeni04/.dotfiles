efa29a343751e3a6db94e05b8c9d73307073904b
kmafeni04 <komemafeni944@gmail.com>
2026-03-17 22:50:31 +0100

    Improve clip script

--- .scripts/clip.sh
+++ .scripts/clip.sh
@@ -12,81 +12,82 @@ init() {
   touch "$hist_file"
 }
 
-init
+notify() {
+  local message="$1"
+  notify-send "clip" "$message"
+}
 
 write() {
-  multiline="$1"
-  [ -z $multiline ] && notify-send "Nothing to write" && exit 0
-  grep -Fxq -e "$multiline" "$hist_file"
-  if [ "$?" -eq 0 ]; then
-    line="$(grep -Fxon -m 1 -e "$multiline" "$hist_file" | grep -Po -m 1 --color="never" "^\d+")"
+  local multiline="$1"
+  [ -z $multiline ] && notify "Nothing to write" && exit 1
+  if grep -Fxq -e "$multiline" "$hist_file"; then
+    local line="$(grep -Fxon -m 1 -e "$multiline" "$hist_file" | grep -Po -m 1 --color="never" "^\d+")"
     sed -i ""$line" d" "$hist_file"
   fi
   echo "$multiline" >> "$hist_file"
 }
 
 replace_newline() {
-  clip="$1"
+  local clip="$1"
   [ -z "$clip" ] && exit 0
   # https://github.com/BreadOnPenguins/scripts/blob/1396a09a206dbd6f9a08ffa7a5603e6d55ae5f00/shortcuts-menus/txtcliphist#L15
   # copied cause I don't know sed
-  multiline=$(echo "$clip" | sed ':a;N;$!ba;s/\n/'"$new_line"'/g')
-  echo "$multiline"
+  local multiline=$(echo -n "$clip" | sed ':a;N;$!ba;s/\n/'"$new_line"'/g')
+  echo -n "$multiline"
+}
+
+get_selection() {
+  local prompt="$1"
+  [ -z "$prompt" ] && exit 1
+
+  local temp_file=$(mktemp 2> /dev/null)
+  [ -z "$temp_file" ] && exit 1
+
+  IFS=$'\n' # Set internal field separator to new line
+  for line in $(cat "$hist_file"); do
+    if [[ "$line" == "$img_surrond"* && "$line" == *"$img_surrond.png" ]]; then
+      echo -en "$line\x00icon\x1f$hist_dir/$line\n" >> "$temp_file" # How images get displayed in rofi
+    else
+      echo "$line" >> "$temp_file"
+    fi
+  done
+  local selection=$(tac "$temp_file" | rofi -theme-str "element-icon { size: 100px; }" -dmenu -i -l 3 -p "$prompt")
+  rm "$temp_file"
+  echo -n "$selection"
 }
 
 copy() {
-  clip=$(xclip -o -selection primary | xclip -i -f -selection clipboard 2> /dev/null)
+  local clip=$(xclip -o -selection primary | xclip -i -f -selection clipboard 2> /dev/null)
   if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
     [ -z "$clip" ] && clip=$(wl-paste --primary | wl-copy && wl-paste -n)
     echo -n "$clip" | xclip -i -selection clipboard
   fi
   if [ -z "$clip" ]; then
-    notify-send "Nothing to copy"
-    exit 0
+    notify "Nothing to copy"
+    exit 1
   fi
-  multiline="$(replace_newline "$clip")"
+  local multiline="$(replace_newline "$clip")"
   write "$multiline"
-  notification="Saved to clipboard"
+  notify "Saved to clipboard"
 }
 
 add() {
-  input="$1"
+  local input="$1"
   [ -z "$input" ] && exit 0
-  clip=$(echo -n "$input" | xclip -i -f -selection clipboard 2> /dev/null)
+  local clip=$(echo -n "$input" | xclip -i -f -selection clipboard 2> /dev/null)
   if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
     echo -n "$input" | wl-copy
   fi
-  multiline="$(replace_newline "$clip")"
+  local multiline="$(replace_newline "$clip")"
   write "$multiline"
-  notification="Saved to clipboard"
-}
-
-get_selection() {
-  prompt="$1"
-  [ -z "$prompt" ] && exit 0
-
-  temp_file=$(mktemp 2> /dev/null)
-  [ -z "$temp_file" ] && exit 0
-
-  IFS=$'\n' # Set internal field separator to new line
-  for line in $(cat "$hist_file"); do
-    if [[ "$line" == "$img_surrond"* && "$line" == *"$img_surrond.png" ]]; then
-      echo -en "$line\x00icon\x1f$hist_dir/$line\n" >> $temp_file
-    else
-      echo -n "$line" >> $temp_file
-      echo "" >> $temp_file
-    fi
-  done
-  selection=$(tac "$temp_file" | rofi -theme-str "element-icon { size: 100px; }" -dmenu -i -l 3 -p "$prompt")
-  rm $temp_file
-  echo "$selection"
+  notify "Saved to clipboard"
 }
 
 sel() {
-  selection=$(get_selection "Select clipboard entry:")
+  local selection=$(get_selection "Select clipboard entry:")
   [ -z "$selection" ] && exit 0
 
-  original=$(echo "$selection" | sed "s/$new_line/\n/g")
+  local original=$(echo -n "$selection" | sed "s/$new_line/\n/g")
   if [[ "$selection" == "$img_surrond"* && "$selection" == *"$img_surrond.png" ]]; then
     xclip -selection clipboard -target image/png -i "$hist_dir/$selection"
     [ "$XDG_SESSION_TYPE" == "wayland" ] && wl-copy < "$hist_dir/$selection"
@@ -94,35 +95,36 @@ sel() {
     echo -n "$original" | xclip -i -selection clipboard
     [ "$XDG_SESSION_TYPE" == "wayland" ] && echo -n "$original" | wl-copy
   fi
-  notification="Selected from clipboard"
   write "$selection"
+  notify "Selected from clipboard"
 }
 
 del() {
-  selection=$(get_selection "Delete clipboard entry:")
-  [ -z "$selection" ] && exit 0
+  local selection=$(get_selection "Delete clipboard entry:")
+  [ -z "$selection" ] && exit 1
 
-  line="$(grep -Fxon -m 1 -e "$selection" "$hist_file" | grep -Po -m 1 --color="never" "^\d+" 2> /dev/null)"
-  [ ${line:-0} -le 0 ] && exit 0
+  local line="$(grep -Fxon -m 1 -e "$selection" "$hist_file" | grep -Po -m 1 --color="never" "^\d+" 2> /dev/null)"
+  [ ${line:-0} -le 0 ] && exit 1
   sed -i ""$line"d" "$hist_file"
   if [[ "$selection" == "$img_surrond"* && "$selection" == *"$img_surrond.png" && -f "$hist_dir/$selection" ]]; then
     rm "$hist_dir/$selection"
   fi
-  notification="Deleted from clipboard"
+  notify "Deleted from clipboard"
 }
 
 recopy() {
-  is_img=""
-  img_from=""
+  local is_img=""
+  local img_from=""
+  local multiline=""
+
   if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
     is_img="$(wl-paste --list-types | grep png 2> /dev/null)"
-  fi
-  if [ -n "$is_img" ]; then
-    img_from="wayland"
+    [ -n "$is_img"] && img_from="wayland"
   else
     is_img="$(xclip -selection clipboard -o -t TARGETS | grep png 2> /dev/null)"
-    img_from="x11"
+    [ -n "$is_img"] && img_from="x11"
   fi
+
   if [ -n "$is_img" ]; then
     img_name="$img_surrond-$(date '+Copied_20%y-%m-%d_%H:%M:%S')-$img_surrond.png"
     if [ "$img_from" == "wayland" ]; then
@@ -131,30 +133,28 @@ recopy() {
       xclip -selection clipboard -t image/png -o > "$hist_dir/$img_name"
     fi
     xclip -i -t image/png "$hist_dir/$img_name"
-    [ "$XDG_SESSION_TYPE" == "wayland" ] && wl-copy < "$hist_dir/$img_name";
+    ([ "$XDG_SESSION_TYPE" == "wayland" ] && wl-copy < "$hist_dir/$img_name")
 
     write "$img_name"
-    notification="Image saved to clipboard"
+    notify "Image saved to clipboard"
   else
     clip=$(xclip -o -selection clipboard 2> /dev/null)
     if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
       [ -z "$clip" ] && clip=$(wl-paste -n 2> /dev/null)
-      echo -n "$clip" | xclip -i -selection clipboard
     fi
     multiline="$(replace_newline "$clip")"
     write "$multiline"
-    notification="Primary selection saved to clipboard"
+    notify "Primary selection saved to clipboard"
   fi
 }
 
+init
 case "$1" in
 copy) copy ;;
 recopy) recopy ;;
 add) add "$2" ;;
 sel) sel ;;
 del) del ;;
-clear) rm -rf "$hist_dir" && init && notification="Clipboard cleared" ;;
+clear) rm -rf "$hist_dir" && init && notify "Clipboard cleared" ;;
 *) exit 1 ;;
 esac
-
-[ -z "$notification" ] || notify-send "$notification"
