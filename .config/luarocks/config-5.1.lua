1345b3fb7ec7a802584f5e931df332381ec9eabc
kmafeni04 <komemafeni944@gmail.com>
2026-03-19 11:40:00 +0100

    Add luarocks config

---
+++ .config/luarocks/config-5.1.lua
@@ -0,0 +1,12 @@
+local_by_default = true
+home_tree = "/home/kome/.config/luarocks"
+rocks_trees = {
+  {
+    name = "user",
+    root = "/home/kome/.config/luarocks",
+  },
+  {
+    name = "system",
+    root = "/usr",
+  },
+}
