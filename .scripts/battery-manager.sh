833c49a750f63245e97be57302242e834b6f7d53
kmafeni04 <komemafeni944@gmail.com>
2026-03-22 00:39:59 +0100

    Update battery_manager

--- .scripts/battery-manager.sh
+++ .scripts/battery-manager.sh
@@ -2,27 +2,30 @@
 
 notify() {
   local msg="$1"
-  [ -n "$1" ] && shift
-  local val="$1"
-  [ -n "$1" ] && shift
+  local val="$2"
+  shift 2
   notify-send 'Battery Manager' "$msg" -u "$val" $@
 }
 
 while true; do
-  current_profie=$(powerprofilesctl | grep '*' | sed -E 's/\*//;s/ //;s/://')
+  current_profile=$(powerprofilesctl | grep '*' | sed -E 's/\*//;s/ //;s/://')
   battery_state=$(upower -i "$(upower -e | grep 'BAT')")
 
-  state="$(echo "$battery_state" | grep state | sed 's/ //g' | cut -d : -f 2)"
-  percent="$(echo "$battery_state" | grep -o 'percentage:\s*[0-9]*%' | sed 's/percentage://;s/ //g;s/%//')"
+  state="$(echo "$battery_state" | grep state | sed 's/state://;s/ //g')"
+  percent="$(echo "$battery_state" | grep percentage | sed 's/percentage://;s/ //g;s/%//')"
 
-  if [ "$state" = "charging" ]; then
-    [ ! "$current_profie" = "performace" ] && powerprofilesctl set performace && notify "Profile set to 'Performance'" normal
-  elif [ "$state" = "discharging" ] && [ ${percent:-0} -lt 35 ]; then
-    [ ! "$current_profie" = "power-saver" ] && powerprofilesctl set power-saver && notify "Profile set to 'Power Saver'" normal
-    action=$(notify "Battery Critically Low" critical -A power-options="Check Power Options" -A ignore="Ignore")
-    [ "$action" = "power-options" ] && $HOME/.scripts/rofi-power.sh
-  else
-    [ ! "$current_profie" = "balanced" ] && powerprofilesctl set balanced && notify "Profile set to 'Balanced'" normal
+  if [ "$state" = "charging" ] || [ "$state" = "fully-charged" ]; then
+    [ ! "$current_profile" = "performance" ] && powerprofilesctl set performance && notify "Profile set to 'Performance'" normal
+  elif [ "$state" = "discharging" ]; then
+    if [ ${percent:-0} -lt 60 ] && [ ! "$current_profile" = "power-saver" ]; then
+      powerprofilesctl set power-saver && notify "Profile set to 'Power Saver'" normal
+    fi
+    if [ ${percent:-0} -lt 40 ]; then
+      action=$(notify "Battery Critically Low" critical -A power-options="Check Power Options" -A ignore="Ignore")
+      [ "$action" = "power-options" ] && $HOME/.scripts/rofi-power.sh
+    fi
+  elif [ ! "$current_profile" = "balanced" ]; then
+    powerprofilesctl set balanced && notify "Profile set to 'Balanced'" normal
   fi
 
   sleep 60
