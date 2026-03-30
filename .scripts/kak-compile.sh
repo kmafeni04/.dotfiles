c39312f5d801cc18c38a9e31acbdaa3b72fdcb10
kmafeni04 <komemafeni944@gmail.com>
2026-03-30 20:24:38 +0100

    Remove scripts that have been moved inside editor

--- .scripts/kak-compile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-compile_command="$@"
-[ -z "$1" ] && echo "No command provided" && exit
-
-echo -e ":nop %{/home/kome/.scripts/kak-run.sh '%val{buffile}'} yes '$compile_command'<ret>" | wezterm cli send-text --no-paste
